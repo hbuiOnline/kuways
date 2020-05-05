5e0effc932e44b32c51d36feeff8e5974d1ea2c3
Han Bui <han.bui2894@gmail.com>
2020-05-05 16:36:19 -0500

    Changed email

--- includes/mailservice.inc.php
+++ includes/mailservice.inc.php
@@ -26,8 +26,8 @@ try {
   $mail->isSMTP();                                            // Send using SMTP
   $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
   $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-  $mail->Username   = 'bugtracker2020@gmail.com';                     // SMTP username
-  $mail->Password   = 'Hviponlin3';                               // SMTP password
+  $mail->Username   = 'kuwayatx@gmail.com';                     // SMTP username
+  $mail->Password   = 'Kubenz1109z';                               // SMTP password
   $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
   $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
